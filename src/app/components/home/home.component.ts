dd4e675ba5914cca01d0798027815083edee3b9f
Nipuna Bhanuka <nipunahettiwatta@gamil.com>
2024-01-09 19:55:35 +0530

    string changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  description: any ;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.description = 'A highly motivated and hardworking undergraduate who is keen on learning new technologies and dedicated to accomplishing a certain objective within the given time.'
   }
 
 }
